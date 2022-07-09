29d223ef47755094c2ebe6f406d6ee7893d29285
thjnhotwp1 <leduythinh20001@gmail.com>
2022-07-09 09:30:44 +0700

    add

---
+++ Quanlisinhvien/src/quanlisinhvien/Person.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlisinhvien;
+import java.util.Scanner;
+/**
+ *
+ * @author onthi
+ */
+public class Person {
+    private String name,gender;
+    private int birthday;
+    private String address;
+
+    public Person() {
+    }
+
+    public Person(String name, String gender, int birthday, String address) {
+        this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    Scanner sc = new Scanner(System.in);
+    public void inputInfo(){
+        System.out.println("Nhap ten:");
+        name = sc.nextLine();
+        System.out.println("Nhap gioi tinh:");
+        gender = sc.nextLine();
+        System.out.println("Nhap ngay sinh:");
+        birthday = sc.nextInt();
+        System.out.println("Nhap dia chi:");
+        address = sc.nextLine();
+    }
+    public void showInfo(){
+        System.out.println("Ten la:"+name);
+        System.out.println("Gioi tinh la:"+gender);
+        System.out.println("Ngay sinh la:"+birthday);
+        System.out.println("Dia chi la:"+address);
+    }
+}
